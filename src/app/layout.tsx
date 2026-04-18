86de367c747dbdbf1a75172a0487d111c960b421
Oleksandr Vlasov <drumlife182@gmail.com>
2026-04-18 10:29:34 +0200

    fix: chenged the title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: 'Oleksandr Vlasov - Software Test Engineer',
+  title: 'Oleksandr Vlasov - Backend Python Developer',
   description: 'Portfolio - Oleksandr Vlasov',
   openGraph: {
     type: 'website',
